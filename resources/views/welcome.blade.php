ca4db3f39ba8d8b8f9080e6f3139766f3305058d
Alessandro Celona <er_tako@hotmail.it>
2025-04-20 21:56:38 +0200

    migliorata la homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,20 @@
                         <span class="mainLetterTitle">O</span>learia <span class="mainLetterTitle">M</span>amerto
                     </h1>
                     <h2 class="">Olio Extravergine di alta qualità October EVOO monocultivar OTTOBRATICA</h2>
-                    <x-success-message></x-success-message>
+                    {{-- Testo introduttivo --}}
+                    <p class="mt-4 px-3 px-md-5">
+                        <strong>Benvenuti in Olearia Mamerto</strong>
+                        <br>
+                        Nel cuore della piana di Gioia Tauro, a Oppido Mamertina, coltiviamo con passione l'eccellenza
+                        dell’olio extravergine di oliva calabrese.
+                        Produciamo un EVOO di qualità superiore, pluripremiato e riconosciuto a livello internazionale.
+                        Scopri il gusto autentico della nostra terra.
+                        <br>
+                        <strong>Olearia Mamerto – Tradizione, innovazione e qualità.</strong><br>
+                        👉 Dai un’occhiata alla sezione <strong><a class="custom-link2"
+                                href="{{ route('article.index') }}">I nostri prodotti</a></strong> e
+                        lasciati conquistare dalle nostre eccellenze.
+                    </p>
                 </div>
             </div>
         </div>
@@ -20,21 +33,36 @@
                 <picture>
                     <source srcset="{{ Storage::url('images/fotoprincipale.bmp') }}" type="image/webp">
                     <source srcset="{{ Storage::url('images/fotoprincipale.bmp') }}" type="image/jpeg">
-                    <img src="{{ Storage::url('images/fotoprincipale.bmp') }}" alt="Bottiglia di olio October EVOO con attestato e bicchieri" class="img-fluid" loading="lazy">
+                        <img src="{{ Storage::url('images/fotoprincipale.bmp') }}"
+                        alt="Bottiglia di olio October EVOO con attestato e bicchieri"
+                        class="img-fluid w-75 rounded-3 shadow-sm mx-auto d-block" loading="lazy">
+                    
+
                 </picture>
             </figure>
 
             <article class="col-12 col-md-8 my-5" aria-label="Descrizione azienda Olearia Mamerto">
                 <p>
-                    L’azienda agricola Olearia Mamerto è ubicata nelle colline poste ai piedi del Parco Nazionale dell’Aspromonte, ad un’altitudine pari a circa 300 metri sul livello del mare, nel Comune di Oppido Mamertina (RC).
+                    L’azienda agricola Olearia Mamerto è ubicata nelle colline poste ai piedi del Parco Nazionale
+                    dell’Aspromonte, ad un’altitudine pari a circa 300 metri sul livello del mare, nel Comune di Oppido
+                    Mamertina (RC).
                     <br>
-                    In un’incantevole atmosfera agreste, caratterizzata dalla presenza di imponenti ulivi secolari, le olive della varietà ottobratica vengono accuratamente selezionate da ciascuna pianta nel momento migliore della loro invaiatura, raccolte a mano e, nell’arco di 24 ore, trasportate al frantoio per la molitura.
+                    In un’incantevole atmosfera agreste, caratterizzata dalla presenza di imponenti ulivi secolari, le
+                    olive della varietà ottobratica vengono accuratamente selezionate da ciascuna pianta nel momento
+                    migliore della loro invaiatura, raccolte a mano e, nell’arco di 24 ore, trasportate al frantoio per
+                    la molitura.
                     <br>
-                    L’evoo così prodotto, non filtrato, viene conservato all’interno di serbatoi in acciaio inox con colmatura in azoto e mantenuto ad una temperatura costante.
+                    L’evoo così prodotto, non filtrato, viene conservato all’interno di serbatoi in acciaio inox con
+                    colmatura in azoto e mantenuto ad una temperatura costante.
                     <br>
-                    L’amore e l’attenzione verso l’intero ciclo vitale delle piante e dei loro frutti, il rispetto delle tradizioni unito all’utilizzo delle più innovative tecniche agronomiche consentono all’azienda Olearia Mamerto di realizzare un evoo di categoria superiore, dal sapore fruttato ed intenso, piacevole al palato ed arricchito da sensazioni di vivacità e freschezza e da un tono amaro-piccante, il tutto, sempre, in perfetto equilibrio.
+                    L’amore e l’attenzione verso l’intero ciclo vitale delle piante e dei loro frutti, il rispetto delle
+                    tradizioni unito all’utilizzo delle più innovative tecniche agronomiche consentono all’azienda
+                    Olearia Mamerto di realizzare un evoo di categoria superiore, dal sapore fruttato ed intenso,
+                    piacevole al palato ed arricchito da sensazioni di vivacità e freschezza e da un tono
+                    amaro-piccante, il tutto, sempre, in perfetto equilibrio.
                     <br>
-                    October Evoo è un olio pregiato ed esclusivo, per chi desidera un gusto unico ed inconfondibile, per viziare il palato.
+                    October Evoo è un olio pregiato ed esclusivo, per chi desidera un gusto unico ed inconfondibile, per
+                    viziare il palato.
                 </p>
             </article>
         </div>
